890f84718496d297486918d69da5e62d12c17ca2
Arshdeep Sidhu <arsh@Arshdeeps-MacBook-Pro.local>
2018-09-11 22:29:15 -0400

    working with Flask-RESTful

---
+++ flask_restful_basic1.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+	def get(self):
+		return {"about": "Hello World"}
+
+	def post(self):
+		inpJson = request.get_json()
+		return {"you sent": inpJson}, 201
+
+class Multi(Resource):
+	def get(self, num, num2):
+		# nums = inpStr.split("-")
+		return {"result": num * num2}
+
+api.add_resource(HelloWorld, "/")
+api.add_resource(Multi, "/multi/<int:num>-<int:num2>")
+
+if __name__ == "__main__":
+	app.run(debug=True)
